64030e977c65c405ed87e0c04f117b90d2dee208
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-08-13 17:03:22 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="policyengine_us_data",
+    version="0.1.0",
+    author="PolicyEngine",
+    author_email="hello@policyengine.org",
+    description="A package to create representative microdata for the US",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "policyengine_core",
+    ],
+    dev_requires=[
+        "black",
+        "pytest",
+    ],
+)

c73c447a85a4c6ef693115c403b040bd4b6bb2b9
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-08-14 23:35:17 +0100

    Skip PUF test

--- policyengine_us_data/tests/test_datasets/test_irs_puf.py
+++ policyengine_us_data/tests/test_datasets/test_irs_puf.py
@@ -1,6 +1,6 @@
 import pytest
 
-
+@pytest.mark.skip(reason="This test requires private data.")
 @pytest.mark.parametrize("year", [2015])
 def test_irs_puf_generates(year: int):
     from policyengine_us_data.datasets.puf.irs_puf import IRS_PUF_2015

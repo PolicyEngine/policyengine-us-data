e728f5b1e78f6ceb5bd68fd55419670d4dea3128
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-08-15 11:25:18 +0100

    Re-add evaluation

--- .github/review_pull_request.py
+++ .github/review_pull_request.py
@@ -3,8 +3,6 @@ from policyengine_us_data.data_storage import STORAGE_FOLDER
 from policyengine_us_data.utils.github import set_pr_auto_review_comment
 
 def main():
-
-    set_pr_auto_review_comment("Testing!")
     df = pd.read_csv(STORAGE_FOLDER / "evaluation.csv")
 
     most_recent_rows = df[df.Date == df.Date.max()].sort_values(["Variable", "Time period"]).set_index(["Variable", "Time period"]).Total
@@ -13,12 +11,14 @@ def main():
     diff = (most_recent_rows - second_most_recent_rows)
     # Convert to df
     diff = diff.reset_index()
-    diff = diff[diff.Variable == "household_net_income"]
+    diff = diff[diff.Variable == "household_net_income"].T
     table = diff.to_markdown(index=False)
 
-    review_text = f"""## National projection changes\n\n{table}"""
+    review_text = f"""## National projection changes\n\nThis pull request makes the following changes to economic estimates.\n\n{table}"""
 
     print(review_text)
+
+    set_pr_auto_review_comment(review_text)
 
 if __name__ == "__main__":
     main()

649d909bd142e47ee54ddf92d3c8506a67d8e907
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-08-27 17:46:05 +0200

    Add missing secret

--- docs/pages/Aggregates.py
+++ docs/pages/Aggregates.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.title("Aggregates")
 
+st.write(
+    """The table below shows the totals for calendar year 2024 for the Enhanced CPS dataset variables."""
+)
+
 from policyengine_us import Microsimulation
 from policyengine_us_data import EnhancedCPS_2024
 from policyengine_us_data.datasets.cps.extended_cps import IMPUTED_VARIABLES as FINANCE_VARIABLES

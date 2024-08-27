38c3f55c3393cc167bea5e83a379585b0080b296
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-08-27 15:38:01 +0200

    Add env to step

--- docs/Home.py
+++ docs/Home.py
@@ -11,3 +11,15 @@ st.subheader("What does this repo do?")
 st.write(
     """Principally, this package creates a (partly synthetic) dataset of households (with incomes, demographics and more) that describes the U.S. household sector. This dataset synthesises multiple sources of data (the Current Population Survey, the IRS Public Use File, and administrative statistics) to improve upon the accuracy of **any** of them."""
 )
+
+st.subheader("What does this dataset look like?")
+
+import pandas as pd
+from policyengine_us_data.datasets import EnhancedCPS_2024
+
+df = pd.read_csv(EnhancedCPS_2024().file_path)
+
+household_id = df[df.filing_status__2024 == "JOINT"].person_household_id__2024.values[0]
+people_in_household = df[df.person_household_id__2024 == household_id]
+
+st.dataframe(people_in_household.T, use_container_width=True)

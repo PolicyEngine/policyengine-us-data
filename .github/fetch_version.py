f976db895e819458ecdce46e8d65dfbe5eee5eaf
Anthony Volk <anth.volk@gmail.com>
2024-09-10 02:18:34 +0200

    fix: Remove setuptools_scm, write __version__ script

--- .github/fetch_version.py
+++ .github/fetch_version.py
@@ -1,17 +1,13 @@
-from setuptools_scm import get_version
+from __version__ import __version__
+
 
 def fetch_version():
     try:
-        # You can add additional parameters here to match your pyproject.toml configuration
-        version = get_version(
-            root=".",  # Assuming you're running this from the project root
-            version_scheme="only-version",
-            local_scheme="no-local-version",
-        )
-        return version
+        return __version__
     except Exception as e:
         print(f"Error fetching version: {e}")
         return None
+
 
 if __name__ == "__main__":
     print(fetch_version())
--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ install:
 changelog:
 	build-changelog changelog.yaml --output changelog.yaml --update-last-date --start-from 1.0.0 --append-file changelog_entry.yaml
 	build-changelog changelog.yaml --org PolicyEngine --repo policyengine-us-data --output CHANGELOG.md --template .github/changelog_template.md
-	bump-version changelog.yaml policyengine_us_data/_version.py
+	bump-version changelog.yaml pyproject.toml
 	rm changelog_entry.yaml || true
 	touch changelog_entry.yaml
 

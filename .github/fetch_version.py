85fdf34542407ff7d6db04699d7025eda68223cc
Anthony Volk <anth.volk@gmail.com>
2024-09-18 21:46:43 +0200

    chore: Lint

--- .github/fetch_version.py
+++ .github/fetch_version.py
@@ -1,10 +1,12 @@
 # Note: Action must be run in Python 3.11 or later
 import tomllib
 
+
 def fetch_version():
     with open("pyproject.toml", "rb") as f:
         pyproject = tomllib.load(f)
     return pyproject["project"]["version"]
+
 
 if __name__ == "__main__":
     print(fetch_version())

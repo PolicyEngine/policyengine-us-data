abf512e9c45315d4a44010dc4b155026a0faf886
PavelMakarchuk <pavel.ma99@gmail.com>
2024-09-19 18:50:17 +0400

    init

--- policyengine_us_data/datasets/__init__.py
+++ policyengine_us_data/datasets/__init__.py
@@ -15,5 +15,6 @@ from .cps import (
     ReweightedCPS_2024,
 )
 from .puf import PUF_2015, PUF_2021, PUF_2024, IRS_PUF_2015
+from .acs import ACS_2022
 
 DATASETS = [CPS_2022, PUF_2021, CPS_2024, EnhancedCPS_2024, ACS_2022]

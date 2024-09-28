91beb1649df2f0041f4683e225561f5ac58c000c
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-09-28 13:48:30 +0100

    Calibrate negative income totals

--- policyengine_us_data/datasets/__init__.py
+++ policyengine_us_data/datasets/__init__.py
@@ -18,4 +18,11 @@ from .cps import (
 from .puf import PUF_2015, PUF_2021, PUF_2024, IRS_PUF_2015
 from .acs import ACS_2022
 
-DATASETS = [CPS_2022, PUF_2021, CPS_2024, EnhancedCPS_2024, ACS_2022]
+DATASETS = [
+    CPS_2022,
+    PUF_2021,
+    CPS_2024,
+    EnhancedCPS_2024,
+    ACS_2022,
+    Pooled_3_Year_CPS_2023,
+]

b8c5575a6065faca2d861ce6926f39cd998d5187
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-10-06 13:41:37 +0100

    Change download folder

--- policyengine_us_data/storage/download_public_prerequisites.py
+++ policyengine_us_data/storage/download_public_prerequisites.py
@@ -1,19 +1,18 @@
 from policyengine_us_data.utils.github import download
 from pathlib import Path
-
-FOLDER = Path(__file__).parent
+from policyengine_us_data.storage import STORAGE_FOLDER
 
 download(
     "PolicyEngine",
     "policyengine-us-data",
     "release",
     "soi.csv",
-    FOLDER / "soi.csv",
+    STORAGE_FOLDER / "soi.csv",
 )
 download(
     "PolicyEngine",
     "policyengine-us-data",
     "release",
     "np2023_d5_mid.csv",
-    FOLDER / "np2023_d5_mid.csv",
+    STORAGE_FOLDER / "np2023_d5_mid.csv",
 )

d5333ff148b1d16a8e4a112776a5b72e2071776c
Anthony Volk <anth.volk@gmail.com>
2025-03-17 20:55:21 -0400

    feat: Add county FIPS dataset export code

--- policyengine_us_data/geography/__init__.py
+++ policyengine_us_data/geography/__init__.py
@@ -5,4 +5,9 @@ ZIP_CODE_DATASET_PATH = (
     Path(__file__).parent.parent / "geography" / "zip_codes.csv.gz"
 )
 
+COUNTY_FIPS_DATASET_PATH = (
+    Path(__file__).parent.parent / "geography" / "county_fips.csv.gz"
+)
+
 ZIP_CODE_DATASET = pd.read_csv(ZIP_CODE_DATASET_PATH, compression="gzip")
+COUNTY_FIPS_DATASET = pd.read_csv(COUNTY_FIPS_DATASET_PATH, compression="gzip")

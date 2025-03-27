a043ddbf26c037ec6a87b4eefea635eb8f7c1213
Anthony Volk <anth.volk@gmail.com>
2025-03-26 17:42:00 -0700

    chore: Add relevant comment, remove deprecated filepath

--- policyengine_us_data/geography/__init__.py
+++ policyengine_us_data/geography/__init__.py
@@ -6,10 +6,6 @@ ZIP_CODE_DATASET_PATH = (
     Path(__file__).parent.parent / "geography" / "zip_codes.csv.gz"
 )
 
-COUNTY_FIPS_DATASET_PATH = (
-    Path(__file__).parent.parent / "geography" / "county_fips.csv.gz"
-)
-
 # Avoid circular import error when -us-data is initialized
 if os.path.exists(ZIP_CODE_DATASET_PATH):
     ZIP_CODE_DATASET = pd.read_csv(ZIP_CODE_DATASET_PATH, compression="gzip")

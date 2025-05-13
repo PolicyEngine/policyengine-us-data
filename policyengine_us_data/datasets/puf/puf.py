769a0097292a4565f8da5d9650e94e27f9c2e84b
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-05-13 14:08:07 +0100

    Add notebook in comment

--- policyengine_us_data/datasets/puf/puf.py
+++ policyengine_us_data/datasets/puf/puf.py
@@ -204,6 +204,8 @@ def preprocess_puf(puf: pd.DataFrame) -> pd.DataFrame:
     # Ignore cmbtp (estimate of AMT income not in AGI)
     # Ignore k1bx14s and k1bx14p (partner self-employment income included in partnership and S-corp income)
     qbi = np.maximum(0, puf.E00900 + puf.E26270 + puf.E02100 + puf.E27200)
+    # 10.1% passthrough rate for W2 wages hits the JCT tax expenditure target for QBID
+    # https://gist.github.com/nikhilwoodruff/262c80b8b17935d6fb8544647143b854
     W2_WAGES_SCALE = 0.101
     puf["w2_wages_from_qualified_business"] = qbi * W2_WAGES_SCALE
 

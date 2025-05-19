14a6515c632cc8972942010486fe326c78b969ac
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2025-05-19 16:34:30 +0100

    Update data files in Google Cloud Buckets on publish (#249)
    * Update data files in Google Cloud Buckets on publish
    Fixes #248
    
    * Remove testing code
    
    * Add Google auth
    
    * Adjust job permissions
    
    * Update permissions
    
    * Remove auth step from PR action
    
    * Update data files in Google Cloud Buckets on publish
    Fixes #248

--- policyengine_us_data/storage/upload_completed_datasets.py
+++ policyengine_us_data/storage/upload_completed_datasets.py
@@ -5,10 +5,20 @@ from policyengine_us_data.datasets import (
 )
 from policyengine_us_data.storage import STORAGE_FOLDER
 from policyengine_us_data.utils.huggingface import upload
+from google.cloud import storage
 
 
 def upload_datasets():
-    for dataset in [EnhancedCPS_2024, Pooled_3_Year_CPS_2023, CPS_2023]:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("policyengine-us-data")
+
+    datasets_to_upload = [
+        EnhancedCPS_2024,
+        Pooled_3_Year_CPS_2023,
+        CPS_2023,
+    ]
+
+    for dataset in datasets_to_upload:
         dataset = dataset()
         if not dataset.exists:
             raise ValueError(
@@ -19,6 +29,13 @@ def upload_datasets():
             dataset.file_path,
             "policyengine/policyengine-us-data",
             dataset.file_path.name,
+        )
+
+        blob = dataset.file_path.name
+        blob = bucket.blob(blob)
+        blob.upload_from_filename(dataset.file_path)
+        print(
+            f"Uploaded {dataset.file_path.name} to GCS bucket policyengine-us-data."
         )
 
 

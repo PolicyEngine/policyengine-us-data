635d7f01ef3c17ccbc53b865e1c784e3bf1cb8c8
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-05-20 10:34:54 +0100

    Move GCP auth code to main action

--- test_upload.py
+++
@@ -1,8 +0,0 @@
-
-from google.cloud import storage
-
-storage_client = storage.Client()
-bucket = storage_client.bucket("policyengine-us-data")
-blob = "README.md"
-blob = bucket.blob(blob)
-blob.upload_from_filename("README.md")

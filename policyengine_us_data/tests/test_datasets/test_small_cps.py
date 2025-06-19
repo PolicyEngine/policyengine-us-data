848f3735eeea3e93c39dbfb16d622e87dc8249aa
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-06-19 10:09:14 +0100

    Rename test

--- policyengine_us_data/tests/test_datasets/test_small_cps.py
+++ policyengine_us_data/tests/test_datasets/test_small_cps.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 @pytest.mark.parametrize("year", [2024])
-def test_policyengine_cps_loads(year: int):
+def test_small_cps_loads(year: int):
     from policyengine_core.data import Dataset
     from policyengine_us_data.storage import STORAGE_FOLDER
     from policyengine_us import Microsimulation

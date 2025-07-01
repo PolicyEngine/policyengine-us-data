efc2e2b202b20430a6f33533793a2d28fd704771
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-07-01 17:22:42 +0100

    Cut test time

--- policyengine_us_data/tests/test_datasets/test_enhanced_cps.py
+++ policyengine_us_data/tests/test_datasets/test_enhanced_cps.py
@@ -1,34 +1,6 @@
 import pytest
 
 
-@pytest.mark.parametrize("year", [2024])
-def test_policyengine_cps_generates(year: int):
-    from policyengine_us_data.datasets.cps import EnhancedCPS_2024
-
-    dataset_by_year = {
-        2024: EnhancedCPS_2024,
-    }
-
-    dataset_by_year[year](require=True)
-
-
-@pytest.mark.parametrize("year", [2024])
-def test_policyengine_cps_loads(year: int):
-    from policyengine_us_data.datasets.cps import EnhancedCPS_2024
-
-    dataset_by_year = {
-        2024: EnhancedCPS_2024,
-    }
-
-    dataset = dataset_by_year[year]
-
-    from policyengine_us import Microsimulation
-
-    sim = Microsimulation(dataset=dataset)
-
-    assert not sim.calculate("household_net_income").isna().any()
-
-
 def test_ecps_has_mortgage_interest():
     from policyengine_us_data.datasets.cps import EnhancedCPS_2024
     from policyengine_us import Microsimulation
@@ -68,8 +40,7 @@ def test_ecps_replicates_jct_tax_expenditures():
     )
 
 
-@pytest.skip
-def test_ecps_replicates_jct_tax_expenditures_full():
+def deprecated_test_ecps_replicates_jct_tax_expenditures_full():
     from policyengine_us import Microsimulation
     from policyengine_core.reforms import Reform
     from policyengine_us_data.datasets import EnhancedCPS_2024

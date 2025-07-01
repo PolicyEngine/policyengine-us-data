af6b697943a1630419a13288c0c6d8d70fd49df6
baogorek <baogorek@gmail.com>
2025-07-01 14:48:47 -0400

    changelog and removing us dependency

--- policyengine_us_data/storage/pull_snap_state_targets.py
+++ policyengine_us_data/storage/pull_snap_state_targets.py
@@ -2,9 +2,63 @@ import requests
 import zipfile
 import io
 import pandas as pd
-import us
 
 from policyengine_us_data.storage import STORAGE_FOLDER
+
+
+STATE_NAME_TO_FIPS = {
+    "Alabama": "01",
+    "Alaska": "02",
+    "Arizona": "04",
+    "Arkansas": "05",
+    "California": "06",
+    "Colorado": "08",
+    "Connecticut": "09",
+    "District of Columbia": "11",
+    "Delaware": "10",
+    "Florida": "12",
+    "Georgia": "13",
+    "Hawaii": "15",
+    "Idaho": "16",
+    "Illinois": "17",
+    "Indiana": "18",
+    "Iowa": "19",
+    "Kansas": "20",
+    "Kentucky": "21",
+    "Louisiana": "22",
+    "Maine": "23",
+    "Maryland": "24",
+    "Massachusetts": "25",
+    "Michigan": "26",
+    "Minnesota": "27",
+    "Mississippi": "28",
+    "Missouri": "29",
+    "Montana": "30",
+    "Nebraska": "31",
+    "Nevada": "32",
+    "New Hampshire": "33",
+    "New Jersey": "34",
+    "New Mexico": "35",
+    "New York": "36",
+    "North Carolina": "37",
+    "North Dakota": "38",
+    "Ohio": "39",
+    "Oklahoma": "40",
+    "Oregon": "41",
+    "Pennsylvania": "42",
+    "Rhode Island": "44",
+    "South Carolina": "45",
+    "South Dakota": "46",
+    "Tennessee": "47",
+    "Texas": "48",
+    "Utah": "49",
+    "Vermont": "50",
+    "Virginia": "51",
+    "Washington": "53",
+    "West Virginia": "54",
+    "Wisconsin": "55",
+    "Wyoming": "56",
+}
 
 
 def extract_usda_snap_data(year=2023):
@@ -74,13 +128,10 @@ def extract_usda_snap_data(year=2023):
 
     results_df = pd.concat(tab_results)
 
-    state_equivs = us.states.STATES + [us.states.DC]
-    fips_lookup = {state.name: state.fips.zfill(2) for state in state_equivs}
-
     df_states = results_df.loc[
-        results_df["State"].isin(fips_lookup.keys())
+        results_df["State"].isin(STATE_NAME_TO_FIPS.keys())
     ].copy()
-    df_states["STATE_FIPS"] = df_states["State"].map(fips_lookup)
+    df_states["STATE_FIPS"] = df_states["State"].map(STATE_NAME_TO_FIPS)
     df_states = (
         df_states.loc[~df_states["STATE_FIPS"].isna()]
         .sort_values("STATE_FIPS")
@@ -93,7 +144,7 @@ def extract_usda_snap_data(year=2023):
 
 def main() -> None:
     out_dir = STORAGE_FOLDER
-    state_df = extract_usda_snap_data(2023)
+    state_df = extract_usda_snap_data(2024)
     state_df.to_csv(out_dir / "snap_state.csv", index=False)
 
 

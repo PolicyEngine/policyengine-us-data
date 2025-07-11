eb96cd5f706b0b718c39e36fa4fd1854bb3e3b0d
baogorek <baogorek@gmail.com>
2025-07-11 16:53:57 -0400

    trying everything

--- policyengine_us_data/datasets/cps/cps.py
+++ policyengine_us_data/datasets/cps/cps.py
@@ -2013,7 +2013,7 @@ if __name__ == "__main__":
     if test_lite:
         CPS_2023().generate()
         CPS_2024().generate()
-        print(2 + 0)
+        print(2 + 9)
     else:
         CPS_2021().generate()
         CPS_2022().generate()

31118bc0e00334cf6cbbfd7e6c62bb472a8fd364
Max Ghenis <mghenis@gmail.com>
2025-07-30 19:49:33 -0400

    Fix imports to use microimpute.utils.qrf instead of microimpute.models.qrf
    The utils.qrf.QRF has the same interface as our old QRF implementation, while models.qrf.QRF has a different interface designed for the Imputer framework.

--- policyengine_us_data/datasets/cps/extended_cps.py
+++ policyengine_us_data/datasets/cps/extended_cps.py
@@ -5,7 +5,7 @@ from policyengine_us_data.datasets.cps.cps import *
 from policyengine_us_data.datasets.puf import *
 import pandas as pd
 import os
-from microimpute.models.qrf import QRF
+from microimpute.utils.qrf import QRF
 import time
 import logging
 

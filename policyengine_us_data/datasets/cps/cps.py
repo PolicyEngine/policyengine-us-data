cbfc58c9a4c5a8eb0287f268d0d267aa40a7b7f3
Max Ghenis <mghenis@gmail.com>
2025-08-09 07:04:10 -0400

    Replace 50% downsampling with larger L0 penalty
    - Remove frac=0.5 from CPS_2019 through CPS_2023 classes
    - Increase L0 penalty from 2.6445e-07 to 1.0e-06 (~4x)
    - Achieves similar computational efficiency through learned sparsity
    - Preserves important observations instead of random removal
    
    Fixes #427

--- policyengine_us_data/datasets/cps/cps.py
+++ policyengine_us_data/datasets/cps/cps.py
@@ -1972,7 +1972,6 @@ class CPS_2019(CPS):
     previous_year_raw_cps = CensusCPS_2018
     file_path = STORAGE_FOLDER / "cps_2019.h5"
     time_period = 2019
-    frac = 0.5
 
 
 class CPS_2020(CPS):
@@ -1982,7 +1981,6 @@ class CPS_2020(CPS):
     previous_year_raw_cps = CensusCPS_2019
     file_path = STORAGE_FOLDER / "cps_2020.h5"
     time_period = 2020
-    frac = 0.5
 
 
 class CPS_2021(CPS):
@@ -1992,7 +1990,6 @@ class CPS_2021(CPS):
     previous_year_raw_cps = CensusCPS_2020
     file_path = STORAGE_FOLDER / "cps_2021_v1_6_1.h5"
     time_period = 2021
-    frac = 0.5
 
 
 class CPS_2022(CPS):
@@ -2002,7 +1999,6 @@ class CPS_2022(CPS):
     previous_year_raw_cps = CensusCPS_2021
     file_path = STORAGE_FOLDER / "cps_2022_v1_6_1.h5"
     time_period = 2022
-    frac = 0.5
 
 
 class CPS_2023(CPS):
@@ -2012,7 +2008,6 @@ class CPS_2023(CPS):
     previous_year_raw_cps = CensusCPS_2022
     file_path = STORAGE_FOLDER / "cps_2023.h5"
     time_period = 2023
-    frac = 0.5
 
 
 class CPS_2024(CPS):

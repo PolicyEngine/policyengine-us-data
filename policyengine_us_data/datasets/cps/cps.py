5ed389414ec32ea05cb690f55c392b8ceef481c0
Max Ghenis <mghenis@gmail.com>
2025-08-09 07:58:30 -0400

    Use combination of 75% sampling and L0 penalty
    Tests were failing with full data. Using a combination approach:
    - 75% random downsampling (less aggressive than before)
    - L0 penalty to further optimize which weights to keep
    This should give us ~30k households initially, with L0 reducing
    to target 20-25k based on importance.

--- policyengine_us_data/datasets/cps/cps.py
+++ policyengine_us_data/datasets/cps/cps.py
@@ -1972,6 +1972,7 @@ class CPS_2019(CPS):
     previous_year_raw_cps = CensusCPS_2018
     file_path = STORAGE_FOLDER / "cps_2019.h5"
     time_period = 2019
+    frac = 0.75  # Reduced downsampling, rely more on L0
 
 
 class CPS_2020(CPS):
@@ -1981,6 +1982,7 @@ class CPS_2020(CPS):
     previous_year_raw_cps = CensusCPS_2019
     file_path = STORAGE_FOLDER / "cps_2020.h5"
     time_period = 2020
+    frac = 0.75  # Reduced downsampling, rely more on L0
 
 
 class CPS_2021(CPS):
@@ -1990,6 +1992,7 @@ class CPS_2021(CPS):
     previous_year_raw_cps = CensusCPS_2020
     file_path = STORAGE_FOLDER / "cps_2021_v1_6_1.h5"
     time_period = 2021
+    frac = 0.75  # Reduced downsampling, rely more on L0
 
 
 class CPS_2022(CPS):
@@ -1999,6 +2002,7 @@ class CPS_2022(CPS):
     previous_year_raw_cps = CensusCPS_2021
     file_path = STORAGE_FOLDER / "cps_2022_v1_6_1.h5"
     time_period = 2022
+    frac = 0.75  # Reduced downsampling, rely more on L0
 
 
 class CPS_2023(CPS):
@@ -2008,6 +2012,7 @@ class CPS_2023(CPS):
     previous_year_raw_cps = CensusCPS_2022
     file_path = STORAGE_FOLDER / "cps_2023.h5"
     time_period = 2023
+    frac = 0.75  # Reduced downsampling, rely more on L0
 
 
 class CPS_2024(CPS):
